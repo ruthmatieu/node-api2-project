fb945a660e5850594cce97f24e1f3b5645c8ee4b
matieuruth <matieu.ruth@gmail.com>
2021-03-03 07:59:25 -0500

    created get and post endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,91 @@
 // implement your posts router here
+const express = require('express');
+const db = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome'
+    })
+})
+
+router.get('/api/posts', (req, res) => {
+    db.find()
+        .then((posts) => {
+            res.status(200).json(posts)
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: "The posts information could not be retrieved"
+            });
+        })
+});
+
+router.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.findById(id)
+        .then((post) => {
+            if(post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: `Sorry, that post doesn't exist.`
+                })
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: `The post information could not be retrieved.`
+            })
+        })
+});
+
+router.post('/api/posts', (req, res) => {
+    const title = req.body.title;
+    const contents = req.body.contents;
+
+    if(!title || !contents) {
+        return res.status(400).json({
+            message: 'Please provide title and contents for the post.'
+        })
+    }
+    db.insert(req.body)
+        .then((newPost) => {
+            res.status(201).json(newPost)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                message: 'There was an error while saving the post to the database.'
+            })
+            
+        })
+});
+
+router.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const post = db.findById(id);
+
+    if(!req.body.title) {
+        res.status(400).json({
+            message: 'Post cannot be empty.'
+        })
+    }
+    if(post){
+        const updatedPost = db.update(id, {
+            post: req.body.post
+        })
+        res.status(400).json(updatedPost);
+    } else {
+        res.status(400).json({
+            message: 'Post not found'
+        })
+    }
+
+})
+
+module.exports = router;
